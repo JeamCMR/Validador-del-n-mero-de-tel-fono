119662f9667f42782823ea3d45bc8992fc9dedd3
COPSTA <tutomx16@gmail.com>
2024-04-22 13:48:03 -0500

    Añade variables necesarias al script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//**VARIABLES */
+
+const userInput = document.getElementById("user-input");
+const buttonCheck = document.getElementById("check-btn");
+const buttonClear = document.getElementById("clear-btn");
+const result = document.getElementById("results-div");
+
