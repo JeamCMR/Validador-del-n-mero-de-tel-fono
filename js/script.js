864653b76c4e172a28abf791e306f72ebc43e2a3
COPSTA <tutomx16@gmail.com>
2024-04-22 15:12:40 -0500

    Añade color y estilos a los resultados de acuerdo al resultado

--- js/script.js
+++ js/script.js
@@ -10,27 +10,34 @@ const result = document.getElementById("results-div");
 /*FUNCIONES Y EVENTOS */
 
 const checkInputValid = () =>{
-    if(!userInput.value){
+    if(!userInput.value || isNaN(parseInt(userInput.value))){
         alert("Please provide a phone number");
+        return;
     }
+    
+    buttonCheakValid(userInput.value);
+    userInput.value="";
 }
+
+const buttonCheakValid = (input) =>{
+    debugger
+    const regexValid = /^(1\s?)?(\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}$/;
+    if(regexValid.test(input)){
+        // console.log("Entra en el if");
+        result.classList.remove("hidden");
+        result.innerHTML += `<p class="valid">Valid US number: ${input}</p>`
+        // console.log(!regexValid.test(userInput.value));
+    }
+    else{
+        debugger
+        result.classList.remove("hidden");
+        result.innerHTML += `<p class="invalid">Invalid US number:${userInput.value}</p>`
+    }
+} 
 
 //Validar
 buttonCheck.addEventListener("click", ()=>{
     checkInputValid();
-    const regexValid = /^(1\s?)?(\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}$/;
-    if(!regexValid.test(userInput.value)){
-        debugger
-        console.log("Entra en el if");
-        result.classList.toggle("hidden");
-         result.innerHTML += `<p>Valid US number: ${userInput.value}</p>`
-    }else{
-        result.classList.toggle("hidden");
-        result.innerHTML += `<p>Invalid US number:" ${userInput.value}</p>`
-        
-        console.log("Entra en el else");
-    }
-    userInput.value="";
 })
 
 
