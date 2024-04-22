85f455f3fe5def213c69126aad1f541cd94172f9
COPSTA <tutomx16@gmail.com>
2024-04-22 15:36:34 -0500

    Cambios en al funcion checkInputValid, cambios en el css al padding del input

--- js/script.js
+++ js/script.js
@@ -5,46 +5,39 @@ const buttonCheck = document.getElementById("check-btn");
 const buttonClear = document.getElementById("clear-btn");
 const result = document.getElementById("results-div");
 
-
-
 /*FUNCIONES Y EVENTOS */
 
-const checkInputValid = () =>{
-    if(!userInput.value || isNaN(parseInt(userInput.value))){
-        alert("Please provide a phone number");
-        return;
-    }
-    
-    buttonCheakValid(userInput.value);
-    userInput.value="";
-}
+const checkInputValid = () => {
+  if (!userInput.value) {
+    alert("Please provide a phone number");
+    return;
+  }
 
+  buttonCheakValid(userInput.value);
+  userInput.value = "";
+};
 
 const clearButton = () => {
-    result.innerHTML = ""
-}
+  result.innerHTML = "";
+};
 
-const buttonCheakValid = (input) =>{
-    debugger
-    const regexValid = /^(1\s?)?(\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}$/;
-    if(regexValid.test(input)){
-
-        result.classList.remove("hidden");
-        result.innerHTML += `<p class="valid">Valid US number:<br> ${input}</p>`
-
-    }
-    else{
-        result.classList.remove("hidden");
-        result.innerHTML += `<p class="invalid">Invalid US number:<br> ${userInput.value}</p>`
-    }
-} 
+const buttonCheakValid = (input) => {
+  const regexValid = /^(1\s?)?(\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}$/;
+  if (regexValid.test(input)) {
+    result.classList.remove("hidden");
+    result.innerHTML += `<p class="valid">Valid US number:<br> ${input}</p>`;
+  } else {
+    result.classList.remove("hidden");
+    result.innerHTML += `<p class="invalid">Invalid US number:<br> ${userInput.value}</p>`;
+  }
+};
 
 //Botton Validar
-buttonCheck.addEventListener("click", ()=>{
-    checkInputValid();
-})
+buttonCheck.addEventListener("click", () => {
+  checkInputValid();
+});
 
 //Boton borrar
-buttonClear.addEventListener("click", ()=>{
-    clearButton();
-})
+buttonClear.addEventListener("click", () => {
+  clearButton();
+});
