12d70bde267a82bcc078873e0b58c7fcca289bf1
COPSTA <tutomx16@gmail.com>
2024-04-22 13:48:41 -0500

    Agrega funcion para validar el input

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ const buttonCheck = document.getElementById("check-btn");
 const buttonClear = document.getElementById("clear-btn");
 const result = document.getElementById("results-div");
 
+
+
+/*FUNCIONES Y EVENTOS */
+
+const checkInputValid = () =>{
+    debugger
+    if(!userInput.value){
+        alert("Please provide a phone number");
+    }
+}
+
